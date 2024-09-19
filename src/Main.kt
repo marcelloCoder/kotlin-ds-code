bfba7753f1b40c9aa93d4a03442055468f08848e
marcelloCoder <deoliveiral767@gmail.com>
2024-09-18 21:22:20 -0300

    initial

---
+++ src/Main.kt
@@ -0,0 +1,24 @@
+fun main(args: Array<String>){
+    var userName : String = "MARCELLO"
+    var value : Int = 760
+    var value2 : Int = 11
+    var valueDouble : Double = 55.4
+    var valueLong : Long = 99L
+    var maxInteger : Int = Int.MAX_VALUE
+    var valueBoolean : Boolean = false
+    println("---- $userName")
+    println("---- $value")
+    println("---- $valueDouble")
+    println("---- $valueLong")
+    println("---- $maxInteger")
+    println("---- $valueBoolean")
+    println("---- $value + $value2")
+    println("---- ${value + value2}")
+    println("---- ${value - value2}")
+    println("---- ${value / value2}")
+    println("---- ${value * value2}")
+    println("---- ${value % value2}")
+
+
+
+}
